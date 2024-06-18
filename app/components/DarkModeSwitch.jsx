a9a892f5d14ed4169c37b2315873c89f72ca289b
mikias-wondim <mikiaswondim0962939288@gmail.com>
2024-06-18 16:51:34 +0300

    Added a theme mode changer using next-themes

---
+++ app/components/DarkModeSwitch.jsx
@@ -0,0 +1,25 @@
+"use client";
+
+import { useTheme } from "next-themes";
+import { MdLightMode, MdDarkMode } from "react-icons/md";
+
+export default function DarkModeSwitch() {
+  const { theme, setTheme, systemTheme } = useTheme();
+  const currentTheme = theme === "system" ? systemTheme : theme;
+
+  return (
+    <div className="">
+      {currentTheme === "dark" ? (
+        <MdLightMode
+          onClick={() => setTheme("light")}
+          className="text-xl cursor-pointer hover:text-amber-500"
+        />
+      ) : (
+        <MdDarkMode
+          onClick={() => setTheme("dark")}
+          className="text-xl cursor-pointer hover:text-amber-500"
+        />
+      )}
+    </div>
+  );
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import Header from "../app/components/Header"
+import Header from "../app/components/Header";
+import Providers from "@/app/Providers";
+
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -18,8 +20,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Header />
-        {children}
+        <Providers>
+          <Header />
+          {children}
+        </Providers>
       </body>
     </html>
   );
